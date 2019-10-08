83729972a8b6b82717c991d373e5337304497039
burbolka <badbird87@gmail.com>
2019-10-08 15:16:56 +0300

    initial commit for doc

---
+++ Dexter.IndexStrategies/Converters/DocToTextConverter.cs
@@ -0,0 +1,24 @@
+﻿namespace Dexter.IndexStrategies.Converters
+{
+    using System;
+    using System.Text;
+
+    public class DocToTextConverter : IPropertyConverter<string, string>
+    {
+        public string Convert(string filePath)
+        {
+            var sb = new StringBuilder();
+
+            try
+            {
+                
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
