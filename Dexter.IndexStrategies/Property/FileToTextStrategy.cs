49245120cb65c2cffb026ddd29ea697a3c4727b2
burbolka <badbird87@gmail.com>
2019-10-08 12:09:48 +0300

    remove unused references

--- Dexter.IndexStrategies/Property/FileToTextStrategy.cs
+++ Dexter.IndexStrategies/Property/FileToTextStrategy.cs
@@ -1,22 +1,15 @@
-﻿using Dexter.Core.Interfaces;
-using Dexter.Core.Models.Config;
-using Dexter.Core.Models.IndexStrategy;
-using Dexter.IndexStrategies.Converters;
-using DocumentFormat.OpenXml.Packaging;
-using DocumentFormat.OpenXml.Wordprocessing;
-using iTextSharp.text.pdf;
-using iTextSharp.text.pdf.parser;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web;
-using Umbraco.Core;
-
-namespace Dexter.IndexStrategies.Property
+﻿namespace Dexter.IndexStrategies.Property
 {
+    using Dexter.Core.Interfaces;
+    using Dexter.Core.Models.Config;
+    using Dexter.Core.Models.IndexStrategy;
+    using Dexter.IndexStrategies.Converters;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Web;
+    using Umbraco.Core;
+
     public class FileToTextStrategy : IPropertyIndexStrategy
     {
         protected string[] IGNORE = new[] { "the", "of", "a", "but", "there", "where", "\n", "for", "and" };
@@ -24,7 +17,6 @@ namespace Dexter.IndexStrategies.Property
         public void Execute(IndexFieldEvent e)
         {
             var filePath = HttpContext.Current.Server.MapPath(e.UmbracoProperty.Value.ToString());
-
             var text = string.Empty;
 
             switch(System.IO.Path.GetExtension(filePath))
@@ -44,28 +36,6 @@ namespace Dexter.IndexStrategies.Property
             }
 
             e.Value = string.Join(" ", text.Split(new[] { ' ' }).Except(IGNORE));
-        }
-
-        private static string ConvertDocToText(byte[] bytes)
-        {
-            var sb = new StringBuilder();
-
-            try
-            {
-                var reader = new PdfReader(bytes);
-                var numberOfPages = reader.NumberOfPages;
-
-                for (var currentPageIndex = 1; currentPageIndex <= numberOfPages; currentPageIndex++)
-                {
-                    sb.Append(PdfTextExtractor.GetTextFromPage(reader, currentPageIndex));
-                }
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine(exception.Message);
-            }
-
-            return sb.ToString();
         }
     }
 }
